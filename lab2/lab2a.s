cbfe4bcb242a8e35eeecd2fdef845034f07b5028
Preston Maness <aggroskater@gmail.com>
2013-06-05 15:22:22 -0500

    Beginning work on lab2a.

---
+++ lab2/lab2a.s
@@ -0,0 +1,7 @@
+; vim: set filetype=asmhc12:                                                                                                     
+
+; This program has us using some of the macros provided by Dr. Stapleton.
+
+#INCLUDE ../HC12TOOLS.INC
+
+
