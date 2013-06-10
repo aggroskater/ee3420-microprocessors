de3a929dad1d0b4f373bbe5783e49c70f4ecd1d1
Preston Maness <aggroskater@gmail.com>
2013-06-09 20:00:41 -0500

    Looks like I've got input validation working, in a sense. The problem is I have lots of branching and I don't know how to branch without chucking the next address on the stack.

--- lab2/lab2b.s
+++ lab2/lab2b.s
@@ -38,7 +38,57 @@ MAIN:
 PASSED1:
 
 	PUTS_SCI0 #PASS
-	RTS
+	ATOI #STR_SEED1,INT_SEED1
+	JSR GET_SEED2
+
+PASSED2:
+
+	JSR OUTPUT
+
+GET_SEED2:
+
+	PUTS_SCI0 #PROMPT_SEED2
+	GETS_SCI0 #STR_SEED2
+	LDX #0
+	JSR CHECK_SEED2
+
+CHECK_SEED2:
+
+	LDAA STR_SEED2,X
+	CMPA #58
+	BHI FAIL3
+	LDAA STR_SEED2,X
+	CMPA #47
+	BLO FAIL3
+	INX
+	CPX #10
+	BEQ FAIL3
+	JSR CHECK_SEED2
+
+FAIL3:
+
+	LDAA #NULL
+	CMPA STR_SEED2,X
+	BEQ FOUND_TERMINATOR2
+
+FAIL4:
+
+	PRINTF_DBUG12 #BAD, #STR_SEED2
+	JSR GET_SEED2	
+
+FOUND_TERMINATOR2:
+
+	CPX #0
+	BNE DIFFCHECK		; not empty string. check not equal to seed1.
+	JSR FAIL4		; empty string. try again.
+
+DIFFCHECK:
+
+	ATOI #STR_SEED2,INT_SEED2
+	LDY INT_SEED2
+	CMPY INT_SEED1
+	BEQ FAIL4
+	JSR PASSED2
 
 GET_SEED1:
 
@@ -50,7 +100,7 @@ GET_SEED1:
 
 CHECK_SEED1:
 
-	PUTS_SCI0 #INCHECK
+;	PUTS_SCI0 #INCHECK
 ;	LDAA #58
 ;	CMPA STR_SEED1,X
 	LDAA STR_SEED1,X
@@ -68,22 +118,27 @@ CHECK_SEED1:
 	
 FAIL1:
 
-	PUTS_SCI0 #INFAIL
+;	PUTS_SCI0 #INFAIL
 	LDAA #NULL
 	CMPA STR_SEED1,X
-	BEQ FOUND_TERMINATOR	; found terminator. not an invalid char.
-	PUTS_SCI0 #PROMPT_FAIL
+	BEQ FOUND_TERMINATOR1	; found terminator. not an invalid char.
+FAIL2:
+;	PUTS_SCI0 #PROMPT_FAIL
 	PRINTF_DBUG12 #BAD, #STR_SEED1
 	LDAB STR_SEED1,X
 	JSR GET_SEED1
 
-FOUND_TERMINATOR:
+FOUND_TERMINATOR1:
 
-	PUTS_SCI0 #INFOUND
+;	PUTS_SCI0 #INFOUND
 	CPX #0			; check for no-input
 	LBNE PASSED1		; found null terminator. wasn't first char. 
-	RTS			; It WAS first char. user didn't input anything.
+	JSR FAIL2		; It WAS first char. user didn't input anything.
 
+OUTPUT:
+
+	PUTS_SCI0 #PASS
+	RTS
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
